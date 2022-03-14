f8083f70e5101d6ea0178515e18112f5e82e94e1
Juan Pablo Sánchez Rey <juanpablosanchezrey22@gmail.com>
2022-03-14 09:00:35 -0500

    TwoWayBanding

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,17 @@
 import { Component } from '@angular/core';
-
+import { Persona } from './persona.model';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
-  title = 'listado-personas';
+  titulo = 'listado de personas';
+  personas: Persona[] = [
+    new Persona('Juan', 'Perez'),
+    new Persona('Laura', 'Juare'),
+    new Persona('Jesus', 'Arias'),
+  ];
+
+  nombreInput: string = '';
 }
